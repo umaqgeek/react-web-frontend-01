a8d7033d9d11b165d28668c59f6db6498d356441
Umar Hambaran <omar.hambaran@frogasia.com>
2019-05-11 21:33:25 +0800

    server production

--- src/models/Conn.js
+++ src/models/Conn.js
@@ -1,2 +1,2 @@
 // export const BASE_URL = 'http://localhost:8081';
-export const BASE_URL = 'http://node-server-framework-01-prod.herokuapp.com';
+export const BASE_URL = 'https://node-server-framework-01-prod.herokuapp.com';
